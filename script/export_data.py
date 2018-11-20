0b4d82e876ca99e67a04ea655144150e9a7fa8be
Leon Xu <liangxu@stanford.edu>
2018-11-20 15:26:17 -0800

    Edited preprocessing

--- script/export_data.py
+++ script/export_data.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import argparse
 import pydicom as pd
+import matplotlib.pyplot as plt
 from skimage.transform import *
 
 img_dict = dict()
@@ -10,7 +11,6 @@ data_dir = '../data/'
 training_dir = os.path.join(data_dir, 'training/')
 test_dir = os.path.join(data_dir, 'test/')
 valid_dir = os.path.join(data_dir, 'valid/')
-dir_list = []
 dcm_dir = ''
 ext = 'npy'
 split_num = -3
@@ -19,7 +19,10 @@ split_num = -3
 # parser.add_argument('dcm_path', nargs='?', type=str, default='')
 # args = parser.parse_args()
 # dcm_dir = args.dcm_path
-dcm_dir = '../dataset'
+dcm_dir = '../../24H_DWI_D3/24H_DWI_D3'
+
+invalid_set = ['119', '122', '157', '175', '220']
+reversed_set = ['112', '115', '117', '128', '133', '134', '154', '198', '211', '213', '219', '222', '224', '228', '245', '272', '281', '294', '304', '312', '313', '318', '335']
 
 if not os.path.isdir(data_dir):
     os.mkdir(data_dir)
@@ -31,17 +34,19 @@ if not os.path.isdir(test_dir):
     os.mkdir(test_dir)
 
 def main():
+    dir_list = []
     for subdir in os.listdir(dcm_dir):
         if os.path.isdir(os.path.join(dcm_dir, subdir)):
             dir_list.append(subdir)
+    dir_list = list(set(dir_list) - set(invalid_set))
     dir_list.sort()
     print(dir_list)
     test_set = dir_list[split_num:]
     valid_set = dir_list[split_num * 2:split_num]
     training_set = dir_list[:split_num * 2]
     output_data(test_set, test_dir)
-    output_data(valid_set, valid_dir)
-    output_data(training_set, training_dir)
+    # output_data(valid_set, valid_dir)
+    # output_data(training_set, training_dir)
 
 def preprocess_data(img_arr, mask=True):
     target_size = (256, 256)
@@ -58,7 +63,12 @@ def output_data(dataset, dir):
         dict_id = {'input': [], 'output': []}
         cleaned_dict = {'input': [], 'output': []}
         for maindir, subdirlist, filelist in os.walk(os.path.join(dcm_dir, id), topdown=False):
-            for filename in sorted(filelist):
+            sorted_filelist = sorted(filelist)
+            if id in reversed_set:
+                image_list = sorted_filelist[0: len(sorted_filelist)//2]
+                mask_list = sorted(sorted_filelist[len(sorted_filelist)//2: len(sorted_filelist)], reverse=True)
+                sorted_filelist = image_list + mask_list
+            for filename in sorted_filelist:
                 if ".dcm" in filename.lower():
                     filepath = os.path.join(maindir, filename)
                     RefDs = pd.read_file(filepath)
@@ -76,6 +86,8 @@ def output_data(dataset, dir):
         for i in range(len(cleaned_dict['input'])):
             dict_i = {'input': cleaned_dict['input'][i], 'output': cleaned_dict['output'][i]}
             np.save(os.path.join(dir, '{0}.{1}'.format(id+'_'+str(i+1), ext)), dict_i)
+            plt.imsave(os.path.join(dir, '{0}.{1}'.format(id+'_'+str(i+1)+'_img', 'png')), np.squeeze(dict_i['input'], axis=2))
+            plt.imsave(os.path.join(dir, '{0}.{1}'.format(id+'_'+str(i+1)+'_mask', 'png')), np.squeeze(dict_i['output'], axis=2))
 
 if __name__ == '__main__':
     main()
